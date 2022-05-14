7fbb49a4a47c162cfa4922de037e7d839a517a17
AldianBaim <aldiansyahibrahim5@gmail.com>
2022-05-14 09:56:41 +0700

    Handle status ticket

--- src/views/TicketDetail.vue
+++ src/views/TicketDetail.vue
@@ -5,75 +5,83 @@
         <h4 class="text-center">Tiket tidak ditemukan</h4>
       </div>
       <div v-else class="col-lg-7">
-        <div>
-          Tiket untuk : <strong>{{ ticket.training_event.title }}</strong>
+        <div v-if="ticket.status == 'approved' || ticket.status == 'attended'">
+          <div>
+            Tiket untuk : <strong>{{ ticket.training_event.title }}</strong>
+          </div>
+          <table class="table table-bordered mt-2">
+            <tr>
+              <td width="160">ID Ticket</td>
+              <td width="20">:</td>
+              <td>{{ ticket.ticketcode }}</td>
+            </tr>
+            <tr>
+              <td>Tanggal Daftar</td>
+              <td>:</td>
+              <td>{{ registeredDate }}</td>
+            </tr>
+            <tr>
+              <td>Waktu</td>
+              <td>:</td>
+              <td>
+                <div
+                  v-if="
+                    ticket.training_event.start.substr(0, 10) ===
+                      ticket.training_event.end.substr(0, 10)
+                  "
+                >
+                  {{ convertStartDate }}
+                </div>
+                <div v-else>{{ convertStartDate }} - {{ convertEndDate }}</div>
+                <span
+                  >Pukul {{ ticket.training_event.start.substr(10) }} -
+                  Selesai</span
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>Tempat</td>
+              <td>:</td>
+              <td>
+                <div>{{ ticket.training_event.location }}</div>
+                <a
+                  v-if="ticket.training_event.maps_url != ''"
+                  :href="ticket.training_event.maps_url"
+                  >Lihat map</a
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>Zoom Meeting</td>
+              <td>:</td>
+              <td>
+                <div>
+                  Meeting ID : {{ ticket.training_event.zoom_meeting_id }}
+                </div>
+                <div>Passcode : {{ ticket.training_event.zoom_passcode }}</div>
+                <a :href="ticket.training_event.zoom_link" target="_blank"
+                  >Zoom link</a
+                >
+              </td>
+            </tr>
+            <tr v-if="ticket.training_event.youtube_link != ''">
+              <td>Youtube Live</td>
+              <td>:</td>
+              <td>
+                <a :href="ticket.training_event.youtube_link">{{
+                  ticket.training_event.youtube_link
+                }}</a>
+              </td>
+            </tr>
+            <tr>
+              <td>Kode QR</td>
+              <td>:</td>
+              <td>
+                <img :src="qrCode" alt="Kode QR" />
+              </td>
+            </tr>
+          </table>
         </div>
-        <table class="table table-bordered mt-2">
-          <tr>
-            <td width="160">ID Ticket</td>
-            <td width="20">:</td>
-            <td>{{ ticket.ticketcode }}</td>
-          </tr>
-          <tr>
-            <td>Tanggal Daftar</td>
-            <td>:</td>
-            <td>{{ registeredDate }}</td>
-          </tr>
-          <tr>
-            <td>Waktu</td>
-            <td>:</td>
-            <td>
-              <div
-                v-if="
-                  ticket.training_event.start.substr(0, 10) ===
-                    ticket.training_event.end.substr(0, 10)
-                "
-              >
-                {{ convertStartDate }}
-              </div>
-              <div v-else>{{ convertStartDate }} - {{ convertEndDate }}</div>
-              <span
-                >Pukul {{ ticket.training_event.start.substr(10) }} -
-                Selesai</span
-              >
-            </td>
-          </tr>
-          <tr>
-            <td>Tempat</td>
-            <td>:</td>
-            <td>{{ ticket.training_event.location }}</td>
-          </tr>
-          <tr>
-            <td>Zoom Meeting</td>
-            <td>:</td>
-            <td>
-              <div>
-                Meeting ID : {{ ticket.training_event.zoom_meeting_id }}
-              </div>
-              <div>Passcode : {{ ticket.training_event.zoom_passcode }}</div>
-              <a :href="ticket.training_event.zoom_link" target="_blank"
-                >Zoom link</a
-              >
-            </td>
-          </tr>
-          <tr v-if="ticket.training_event.youtube_link != ''">
-            <td>Youtube Live</td>
-            <td>:</td>
-            <td>
-              <a :href="ticket.training_event.youtube_link">{{
-                ticket.training_event.youtube_link
-              }}</a>
-            </td>
-          </tr>
-          <tr>
-            <td>Kode QR</td>
-            <td>:</td>
-            <td>
-              <img :src="qrCode" alt="Kode QR" />
-            </td>
-          </tr>
-        </table>
-
         <div class="mt-4">
           <strong>Detail Peserta : </strong>
         </div>
@@ -101,12 +109,21 @@
             </td>
           </tr>
           <tr>
-            <td>Status</td>
+            <td>Status Pendaftaran</td>
             <td>:</td>
             <td>
-              <span class="text-primary">{{
-                ticket.status == "approved" && "Pendaftaran Diterima"
-              }}</span>
+              <span v-if="ticket.status == 'approved'" class="text-primary">
+                Pendaftaran Diterima
+              </span>
+              <span v-if="ticket.status == 'attended'" class="text-primary">
+                Telah Mengikuti Acara
+              </span>
+              <span v-if="ticket.status == 'pending'" class="text-primary">
+                Menunggu Konfirmasi
+              </span>
+              <span v-if="ticket.status == 'canceled'" class="text-primary">
+                Pendaftaran Ditolak
+              </span>
             </td>
           </tr>
         </table>
