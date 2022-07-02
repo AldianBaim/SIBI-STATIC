5e7b469a83a9d1ae0186466a9e3ba6c380ef0d70
Ahmad Oriza <ahmadoriza@gmail.com>
2022-07-02 20:22:26 +0700

    Change message

--- src/views/dashboard/PortfolioEdit.vue
+++ src/views/dashboard/PortfolioEdit.vue
@@ -75,7 +75,7 @@
               </div>
               <div class="col-md-12">
                 <div class="form-group">
-                  <label>Lampiran ~ PDF Maks 10MB</label>
+                  <label>Lampiran (PDF/Zip) Maks 10MB</label>
                   <input
                     v-model="portfolio.attachment"
                     type="hidden"
