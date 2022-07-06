dab389fc355a4c2f1eaa6e4bedde0e9f148b8bbf
Ahmad Oriza <ahmadoriza@gmail.com>
2022-07-06 11:34:09 +0700

    Build

--- dist/js/chunk-2d0e8e9e.1a76848b.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e8e9e"],{"8afe":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},r=[],c=t("2877"),l={},a=Object(c["a"])(l,u,r,!1,null,null,null);n["default"]=a.exports}}]);
-//# sourceMappingURL=chunk-2d0e8e9e.1a76848b.js.map
